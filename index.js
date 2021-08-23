e1a410a50003c6e14ef015a343ccadf3b42cb249
denyncrawford <kiquerangelb@gmail.com>
2021-08-23 09:16:33 -0300

    multiplayer

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.send(__dirname + '/public/index.html');
+});
+
+app.get("/socket.io/socket.io.js", (req, res) => {
+  res.sendFile(__dirname + '/node_modules/socket.io-client/dist/socket.io.js');
+});
+
+io.on('connection', (socket) => {
+  socket.on('button', (data) => {
+    io.emit('button', data);
+  })
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
